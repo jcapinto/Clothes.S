d2dd5e08d9189b7e6d331aff924db53f8788ca0e
jcapinto <129993090+jcapinto@users.noreply.github.com>
2023-04-13 10:20:46 +0100

    teste

--- script.js
+++
@@ -1,8 +0,0 @@
-let topBar = document.getElementById("top-bar"),
-    p = document.createElement("p");
-
-    
-p.innerHTML = "Entrega GRÁTIS para compras superiores a 20€";
-topBar.appendChild(p);
-
-
